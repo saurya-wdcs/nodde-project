56453a394a7004b38e5f5937ef4f03428fb301d8
saurya-wdcs <97591918+saurya-wdcs@users.noreply.github.com>
2022-01-18 16:44:03 +0530

    Add files via upload

---
+++ logger.js
@@ -0,0 +1,17 @@
+const EventEmitter = require('events');
+
+var url = 'http://mylogger.io/log';
+
+class Logger extends EventEmitter {
+    log(message) {
+        //Send an HTTP request
+        console.log(message);
+    
+        // Raise an Event 
+        this.emit('messageLogged', { id: 1, url: 'http://'});
+    
+    }
+}
+
+
+module.exports = Logger;
